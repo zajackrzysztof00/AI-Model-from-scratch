fe349de1b7dbf901ac50996659207687903cded0
Krzysztof <zajackrzysztof00@gmail.com>
2024-12-12 19:34:46 +0100

    Update test_all.py
    Added test for new metodes,
    Some tests for prediction function
    Initial code for model training

--- test_all.py
+++ test_all.py
@@ -1,4 +1,4 @@
-from Model import Neuron, Layer
+from Model import Neuron, Layer, Model
 from random import random
 
 def test_neuron_calc_finction():
@@ -18,4 +18,25 @@ def test_layer_output_calc():
     inputs = [random() for i in range(0,test_layer.input_size)]
     test_layer.calc_output(inputs)
 
-    assert len(test_layer.out_values) == test_layer.ouput_size
+    assert len(test_layer.out_values) == test_layer.output_size
+
+global model
+model = Model(16)
+
+def test_model_creation():
+    model.add_layer(32)
+    assert len(model.layers) == 2
+    model.add_layer(16)
+    assert len(model.layers) == 3
+    test_layer = model.layers[-1]
+    for test_neuron in test_layer.neurons:
+        assert test_neuron.calc([random() for i in range(0,test_neuron.input_size)]) <= 1
+
+def test_model_prediction():
+    model.add_layer(32)
+    model.add_layer(16)
+    predictions = model.predict([i for i in range(0,16)])
+    for prediction in predictions:
+        assert prediction <= 1
+    model.train([i for i in range(0,16)],[0 for i in range(0,32)]+[1])
+    print(model.layers[-1].cost)
