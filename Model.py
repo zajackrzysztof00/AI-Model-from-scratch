c58460243707bcf2c1462d36ad3305047fed28fa
Krzysztof <zajackrzysztof00@gmail.com>
2024-12-15 09:56:57 +0100

    Update Model.py
    Model is capable to learn for categorical tasks.
    Input values must have same shape as output (code must be rewriten)

--- Model.py
+++ Model.py
@@ -2,7 +2,10 @@ import math
 from random import random
 
 def sigmoid(value):
-    value = 1/(1+math.e**(-value))
+    try:
+        value = 1/(1+math.e**(-value))
+    except:
+        value = 0
     return value
 
 def derivative_sigmoid(value):
@@ -40,6 +43,18 @@ class Neuron():
     def remember_z_value(self, z_value):
         self.z_value = z_value
 
+    def modify_weights(self):
+        new_weights = []
+        for i, weight in enumerate(self.weights):
+            new_weights.append(weight-self.weight_cost[i])
+        self.weights = new_weights
+
+    def remember_bias_change(self, change):
+        self.bias_change = change
+    
+    def modify_bias(self):
+        self.bias = self.bias - self.bias_change
+
 class Layer():
     def __init__(self, input_size, output_size):
         self.input_size = input_size
@@ -76,34 +91,66 @@ class Model():
         for i, layer in enumerate(self.layers):
             self.layers[-i].calc_cost(train_result)
 
-model = Model(4)
-model.add_layer(4)
-input_val = [2*i for i in range(0,4)]
-dis_out = [1+i for i in range(0,4)]
-model.predict(input_val)
-print(model.layers)
-l_num = len(model.layers)
-for layer in reversed(model.layers):
-    print(layer.out_values)
-    l_num = l_num - 1
-    for n_num , neuron in enumerate(layer.neurons):
-        print(neuron)
-        if l_num-1 >= 0:
-            previous_values =  model.layers[l_num-1].out_values
-        else:
-            previous_values =  input_val
-        z_value = calc_z_value(neuron.weights, previous_values, neuron.bias)
-        neuron.remember_z_value(z_value)
-        for w_num, weight in enumerate(neuron.weights):
+model = Model(8)
+model.add_layer(8)
+model.add_layer(8)
+model.add_layer(8)
+model.add_layer(8)
+input_val = [i for i in range(0,8)]
+#dis_out = [(1+i)/5 for i in range(0,4)]
+dis_out = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8]
+count = 0
+while True:
+    count = count+1
+    prediction = model.predict(input_val)
+    print(prediction)
+    print(dis_out)
+    #print(model.layers)
+    l_num = len(model.layers)
+    for layer in reversed(model.layers):
+        #print(layer.out_values)
+        l_num = l_num - 1
+        for n_num , neuron in enumerate(layer.neurons):
+            #print(neuron)
+            if l_num-1 >= 0:
+                previous_values =  model.layers[l_num-1].out_values
+            else:
+                previous_values =  input_val
+            z_value = calc_z_value(neuron.weights, previous_values, neuron.bias)
+            neuron.remember_z_value(z_value)
+            for w_num, weight in enumerate(neuron.weights):
+                if l_num == len(model.layers)-1:
+                    constant_part = derivative_sigmoid(z_value)*derivative_cost_to_output(neuron.out_value, dis_out[n_num])
+                else:
+                    constant_part = derivative_sigmoid(z_value)*sum([w*
+                                                                    derivative_sigmoid(model.layers[l_num+1].neurons[n_num].z_value)*
+                                                                    model.layers[l_num+1].neurons[n_num].constant_part[i] 
+                                                                    for i, w in enumerate(neuron.weights)])
+                neuron.remember_cosnstant_part(constant_part)
+                #print(w_num)
+                weight_cost_ratio = previous_values[w_num]*constant_part
+                #print(weight_cost_ratio)
+                neuron.remember_weight_cost(weight_cost_ratio)
+    l_num = len(model.layers)
+    for lauer in reversed(model.layers):
+        l_num = l_num - 1
+        for n_num, neuron in enumerate(lauer.neurons):
+            if l_num-1 >= 0:
+                previous_values =  model.layers[l_num-1].out_values
+            else:
+                previous_values =  input_val
+            z_value = neuron.z_value
             if l_num == len(model.layers)-1:
-                constant_part = derivative_sigmoid(z_value)*derivative_cost_to_output(neuron.out_value, dis_out[n_num])
+                    constant_part = derivative_sigmoid(z_value)*derivative_cost_to_output(neuron.out_value, dis_out[n_num])
             else:
                 constant_part = derivative_sigmoid(z_value)*sum([w*
-                                                                 derivative_sigmoid(model.layers[l_num+1].neurons[n_num].z_value)*
-                                                                 model.layers[l_num+1].neurons[n_num].constant_part[i] 
-                                                                 for i, w in enumerate(neuron.weights)])
-            neuron.remember_cosnstant_part(constant_part)
-            print(w_num)
-            weight_cost_ratio = previous_values[w_num]*constant_part
-            print(weight_cost_ratio)
-            neuron.remember_weight_cost(weight_cost_ratio)
+                                                                derivative_sigmoid(model.layers[l_num+1].neurons[n_num].z_value)*
+                                                                model.layers[l_num+1].neurons[n_num].constant_part[i] 
+                                                                for i, w in enumerate(neuron.weights)])
+            bias_change = constant_part
+            neuron.remember_bias_change(bias_change)
+
+    for layer in  reversed(model.layers):
+        for n_num, neuron in enumerate(layer.neurons):
+            neuron.modify_weights()
+            neuron.modify_bias()
